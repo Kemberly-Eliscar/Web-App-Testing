3380c8f54603f9a1e430c0ac1e1f6a32d50cbba5
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-10-10 17:25:55 -0400

    set up components

--- web-app-testing/src/App.js
+++ web-app-testing/src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
-
+import React,{useState} from 'react';
+import Dashboard from "./Components/Dashboard";
+import Display from "./Components/Display";
 
 function App() {
+  const [ballCount, setBallCount] = useState(0);
+  const [strikeCount, setStrikeCount] = useState(0)
+
   return (
     <div className="App">
       <span className="greet">Hello World</span>
